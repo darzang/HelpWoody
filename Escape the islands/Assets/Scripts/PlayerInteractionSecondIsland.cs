17ad499b1d9528edd996916e8c47cf53b3b69ae5
Darzang <mathieu.boez@gmail.com>
2019-03-16 19:34:02 +0000

    Conversations coding
    Coding of most of the conversations in the second island

--- Escape the islands/Assets/Scripts/PlayerInteractionSecondIsland.cs
+++ Escape the islands/Assets/Scripts/PlayerInteractionSecondIsland.cs
@@ -9,46 +9,45 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 
 	public GameObject uiManager;
 
-	public GameObject campfire;
-
 	/********************Conversation Related ********************/
 	int conversationState = 0;
 	bool isTalking = false;
 	bool firstConversation = true;
 
-	/******************** Still to sort out ********************/
-	public static bool questStarted = false;
+	/******************** Quest related ********************/
+	private static bool questStarted = false;
 
-	public int questState = 0;
+	private int questState = 0;
+	public GameObject campfire;
+
+	private GameObject firstWall;
+	private GameObject secondWall;
 
 	// Use this for initialization
 	void Start () {
 		conversationState = 0;
+		firstWall = GameObject.Find ("FirstWall");
+		secondWall = GameObject.Find ("SecondWall");
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		// TO REMOVE 
-
-		if (Input.GetButtonDown ("ButtonQ")) {
-			questState = 1;
-
-		}
-
 		// If we have what it takes to go to next step
-		if (Inventory.rocks >= 8 && Inventory.sticks >= 8 && Inventory.hasMatches) {
+		if (Inventory.rocks >= 8 && Inventory.sticks >= 8 || Input.GetButtonDown ("ButtonQ")) {
 			if (questState == 0) {
 				questState = 1;
 			}
 		}
 
 		if (isTalking) {
-			GameObject.Find ("Player").GetComponent<CharacterController> ().enabled = false;
+			if (GameObject.Find ("Player").GetComponent<CharacterController> ().enabled) {
+				GameObject.Find ("Player").GetComponent<CharacterController> ().enabled = false;
+			}
 			HandleConversation ();
 		} else {
 			RaycastHit hit;
-			if (Physics.Raycast (transform.position, transform.forward, out hit, 3) && hit.collider.gameObject.tag == "Woody") {
+			if (Physics.Raycast (transform.position, transform.forward, out hit, 3) && hit.collider.gameObject.tag == "Woody" && questState != 2) {
 				uiManager.GetComponent<UIManager> ().enableInteractCrosshair ();
 				if (Input.GetButtonDown ("ButtonR")) {
 					isTalking = true;
@@ -56,6 +55,18 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 					uiManager.GetComponent<UIManager> ().disableNoticePanel ();
 					uiManager.GetComponent<UIManager> ().enableConversationPanel ();
 				}
+			} else if (Physics.Raycast (transform.position, transform.forward, out hit, 6) && hit.collider.gameObject.name == "PenguinChef") {
+				Debug.Log ("facing penguin");
+				if (questState == 2) {
+					uiManager.GetComponent<UIManager> ().enableInteractCrosshair ();
+					if (Input.GetButtonDown ("ButtonR")) {
+						isTalking = true;
+						uiManager.GetComponent<UIManager> ().disableInteractCrosshair ();
+						uiManager.GetComponent<UIManager> ().disableNoticePanel ();
+						uiManager.GetComponent<UIManager> ().enableConversationPanel ();
+					}
+				}
+
 			} else {
 				uiManager.GetComponent<UIManager> ().disableInteractCrosshair ();
 			}
@@ -70,6 +81,11 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 		handlePanelForSecondConversation ();
 		handleUserAnswerForSecondConversation ();
 	}
+
+	void handlePenguinConversation () {
+		handlePanelForPenguinConversation ();
+		handleUserAnswerForPenguinConversation ();
+	}
 	void HandleConversation () {
 		switch (questState) {
 			case 0:
@@ -78,10 +94,14 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 			case 1:
 				handleSecondConversation ();
 				break;
+			case 2:
+				handlePenguinConversation ();
+				break;
 
 		}
 	}
 
+	#region User Answers Handling
 	void handleUserAnswerForFirstConversation () {
 		switch (conversationState) {
 			case 0:
@@ -90,11 +110,6 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 				}
 				break;
 			case 1:
-				if (Input.GetButtonDown ("ButtonE")) {
-					conversationState = 2;
-				}
-				break;
-			case 2:
 				if (Input.GetButtonDown ("ButtonE")) {
 					GameObject.Find ("Player").GetComponent<CharacterController> ().enabled = true;
 					uiManager.GetComponent<UIManager> ().disableConversationPanel ();
@@ -124,10 +139,46 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 					isTalking = false;
 					conversationState = 0;
 					campfire.gameObject.SetActive (true);
+					questState = 2;
+					Destroy (firstWall);
 				}
 				break;
 		}
 	}
+
+	void handlePanelForPenguinConversation () {
+		switch (conversationState) {
+			case 0:
+				if (Input.GetButtonDown ("ButtonE")) {
+					conversationState = 1;
+				}
+				break;
+			case 1:
+				if (Input.GetButtonDown ("ButtonE")) {
+					conversationState = 2;
+				}
+				break;
+			case 2:
+				if (Input.GetButtonDown ("ButtonE")) {
+					conversationState = 3;
+				}
+				break;
+			case 3:
+				if (Input.GetButtonDown ("ButtonE")) {
+					GameObject.Find ("Player").GetComponent<CharacterController> ().enabled = true;
+					uiManager.GetComponent<UIManager> ().disableConversationPanel ();
+					isTalking = false;
+					conversationState = 0;
+					questState = 3;
+					Destroy(secondWall);
+				}
+				break;
+		}
+	}
+
+	#endregion
+
+	#region Panel Handling
 
 	void handlePanelForSecondConversation () {
 		switch (conversationState) {
@@ -140,13 +191,13 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 			case 1:
 				setConversationPanelTexts (
 					"Well I didn't see any sign of him, but I've seen that the penguin chef and his friends are back, maybe we should try to ask him",
-					"And which one is the king?"
+					"And which one is it ?"
 				);
 				break;
 			case 2:
 				setConversationPanelTexts (
-					"He's... different, you should be able to notice him without too much trouble",
-					"Okay, I'm on it!"
+					"He's the only one standing on our side of the island, and he's a bit taller and brighter",
+					"Okay, I'll go ask him"
 				);
 				break;
 
@@ -156,7 +207,6 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 	void handlePanelForFirstConversation () {
 		switch (conversationState) {
 			case 0:
-				//First talk
 				uiManager.GetComponent<UIManager> ().disableNegativePanel ();
 				setConversationPanelTexts (
 					"So, Here we are, a bit cold right ? We should start a fire before freezing to death !",
@@ -167,16 +217,39 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 				//Player accepts to help once
 				setConversationPanelTexts (
 					"You could collect some sticks and somes rocks so that we can prepare the campfire, meanwhile I'll investigate on my missing brother",
-					"Anything else ?"
+					"I'm on it!"
+				);
+				break;
+		}
+	}
+
+	void handleUserAnswerForPenguinConversation () {
+		switch (conversationState) {
+			case 0:
+				uiManager.GetComponent<UIManager> ().disableNegativePanel ();
+				setConversationPanelTexts (
+					"Hello there traveller, whats brings you here ?",
+					"Explain the situation"
+				);
+				break;
+			case 1:
+				setConversationPanelTexts (
+					"I'm afraid this man was stealing our fishes when we got back so we had to lock him in a cage until we decide its fate.",
+					"Try to find an agreement"
 				);
 				break;
 			case 2:
 				setConversationPanelTexts (
-					"Oh yeah, my brother brought a chest with him when he left, there may be some matches inside. Find the chest and we won't freeze to death !",
-					"I'm on it !"
+					"This man stole several fish from us, if you find a way to refund them, I could give you the key to the lock",
+					"Offer coconuts and mushrooms instead"
 				);
 				break;
-
+			case 3:
+				setConversationPanelTexts (
+					"That seems like a fair trade, I haven't had coconuts in year ! Here's the key, the cage is a bit further on my left.",
+					"Take the key and go away"
+				);
+				break;
 		}
 	}
 
@@ -185,4 +258,6 @@ public class PlayerInteractionSecondIsland : MonoBehaviour {
 		uiManager.GetComponent<UIManager> ().setPositiveAnswerText (positive);
 		uiManager.GetComponent<UIManager> ().setNegativeAnswerText (negative);
 	}
+
+	#endregion
 }
